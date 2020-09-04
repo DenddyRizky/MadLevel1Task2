ea0e59bfe67aee813b922ec244276a95d6249b3b
Denddy <denddyrizky@gmail.com>
2020-09-04 11:11:55 +0200

    The app is completed and it works

--- app/src/main/java/com/example/madlevel1task2/ConjunctionActivity.kt
+++ app/src/main/java/com/example/madlevel1task2/ConjunctionActivity.kt
@@ -2,6 +2,7 @@ package com.example.madlevel1task2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.madlevel1task2.databinding.ActivityConjunctionBinding
 
 class ConjunctionActivity : AppCompatActivity() {
@@ -12,7 +13,38 @@ class ConjunctionActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityConjunctionBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        initViews()
     }
 
+    private fun initViews(){
+        binding.btSubmit.setOnClickListener(){
+            checkAnswers()
+        }
+    }
 
+    private fun checkAnswers(){
+        var correctAnswer = 0
+        val answer1 = binding.etAnswer1.text.toString()
+        val answer2 = binding.etAnswer2.text.toString()
+        val answer3 = binding.etAnswer3.text.toString()
+        val answer4 = binding.etAnswer4.text.toString()
+
+        if (answer1 == getString(R.string.value_true)){
+            correctAnswer++
+        }
+
+        if (answer2 == getString(R.string.value_false)){
+            correctAnswer++
+        }
+
+        if (answer3 == getString(R.string.value_false)){
+            correctAnswer++
+        }
+
+        if (answer4 == getString(R.string.value_false)){
+            correctAnswer++
+        }
+
+        Toast.makeText(this, getString(R.string.correct,correctAnswer), Toast.LENGTH_SHORT).show()
+    }
 }
